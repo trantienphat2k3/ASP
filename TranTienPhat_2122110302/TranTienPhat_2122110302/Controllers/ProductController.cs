f61f6438b14fe6b4e6cfebd844af94f23957cbd8
trantienphat <trantienphat3003@gmail.com>
2024-08-14 10:58:15 +0700

    first commit

---
+++ TranTienPhat_2122110302/TranTienPhat_2122110302/Controllers/ProductController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TranTienPhat_2122110302.DB;
+
+namespace TranTienPhat_2122110302.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        WebsiteEcomEntities db = new WebsiteEcomEntities();
+        // GET: Prooduct
+        public ActionResult Detail(int id)
+        {
+            var detail = db.Product.Find(id);
+            return View(detail);
+        }
+        //public ActionResult Search(string query)
+        //{
+        //    // Tìm kiếm sản phẩm theo tên (không phân biệt chữ hoa chữ thường)
+        //    var result = Product.Where(p => p.Name.Contains(query, StringComparison.OrdinalIgnoreCase)).ToList();
+        //    return View(result);
+        //}
+
+    }
+}
